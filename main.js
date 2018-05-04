698eccea7cb9f94f7494523fe63753c9b61a19bf
Patrick Coury <couryp@gmail.com>
2018-05-04 08:24:27 -0700

    I believe this is working, now with lofi bg

--- main.js
+++ main.js
@@ -1,22 +1,55 @@
 if (window.File && window.FileReader && window.FileList && window.Blob) {
-  console.log('works')
+  console.log('That is a nice computer. File APIs supported.')
 } else {
   alert('The File APIs are not fully supported in this browser.');
 }
 
+let files = document.querySelector('#files')
+let body = document.querySelector('#body')
+
+files.addEventListener('change', handleFileSelect, false)
+files.addEventListener('change', handleFileSelect2, false)
 
 function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
+  let result = ""
+  let evtFiles = evt.target.files
+  for(let i = 0; i < evtFiles.length; i++) {
+    let reader = new FileReader()
+    reader.readAsText(evtFiles[i], 'application/json')
+    reader.onload = (evt) => {
+      let newFiles = JSON.parse(evt.target.result)
+      newFiles.forEach(ele => {
+        console.log(ele)
+        result += helper(ele)
+      })
+      body.innerHTML = result
+    }
+  }
+}
 
-    // files is a FileList of File objects. List some properties.
+function handleFileSelect2(evt) {
+    let filesTest = evt.target.files
     var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
+    for (var i = 0, f; f = filesTest[i]; i++) {
       output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
                   f.size, ' bytes, last modified: ',
                   f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
                   '</li>');
     }
     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-  }
+}
 
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+function helper(ele) {
+  let openTag = `<${ele.tag}>`
+  let closeTag = `</${ele.tag}>`
+  let strIntrpltn = ""
+  if (typeof ele.content === "object" && ele.content.length) {
+    strIntrpltn += ele.content.map(e => helper(e)).join('')
+  } else if (typeof ele.content === 'object') {
+    strIntrpltn += helper(ele.content)
+  } else {
+    console.log('meow',ele.content)
+    strIntrpltn += `${ele.content}`
+  }
+  return `${openTag} ${strIntrpltn} ${closeTag}`
+}
